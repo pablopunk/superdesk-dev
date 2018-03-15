31cdba44a750a4447bf811371f91e0ed1b9649ee
Pablo Varela <pablovarela182@gmail.com>
2018-03-15 16:39:18 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+```sh
+ln -sf $PWD/superdesk-dev.sh /usr/local/bin/superdesk-dev
+superdesk-dev status
+superdesk-dev start
+superdesk-dev restart
+superdesk-dev stop
+```
---
+++ superdesk-dev.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function start {
+  cd "$HOME/src/superdesk${1}/server" && \
+    . $HOME/.pyvenv/bin/activate && \
+    honcho start &
+
+  echo $! > /tmp/superdesk-server-pid
+
+  cd $HOME/src/superdesk-client-core && \
+    npx grunt &
+
+  echo $! > /tmp/superdesk-client-pid
+
+  open http://localhost:9000
+}
+
+function stop {
+  ps aux | grep [p]yvenv | tr -s ' ' | cut -d' ' -f2 | xargs kill -9
+  ps aux | grep [w]s.py | tr -s ' ' | cut -d' ' -f2 | xargs kill -9
+  killall grunt
+}
+
+function status {
+  server=$(ps aux | grep [p]yvenv)
+  ( [ ! -z "$server" ] && echo "Server up" ) || echo "Server down"
+  client=$(ps aux | grep [g]runt)
+  ( [ ! -z "$client" ] && echo "Client up" ) || echo "Client down"
+}
+
+function restart {
+  stop
+  start $1
+}
+
+# Execute the function $1 witb the argument $2
+$1 $2
