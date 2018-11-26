b7601700757a4741a91167aa6956c0140a226084
pablopunk <pablovarela182@gmail.com>
2018-11-26 19:36:49 +0000

    sd setup!

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ ! "$(whoami)" == "root" ]]
+then
+  echo "Rerun this script as sudo"
+  exit 1
+fi
+
+function add_apt_repositories {
+  wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+  echo "deb https://packages.elastic.co/elasticsearch/2.x/debian stable main" | tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+  add-apt-repository -y ppa:webupd8team/java
+  apt update
+}
+
+function install_apt_deps {
+  apt install -y \
+    mongodb-server \
+    redis-server \
+    elasticsearch \
+    libxmlsec1-dev \
+    pkg-config
+}
+
+add_apt_repositories
+install_apt_deps
