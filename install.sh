9f329e51f21b956224cd87cff27df79ba2c782f4
pablopunk <pablovarela182@gmail.com>
2018-12-03 17:58:22 +0100

    sudo

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sudo ln -sf $PWD/superdesk-dev.sh /usr/local/bin/sd
+
+if [[ ! "$(whoami)" == "root" ]]
+then
+  echo "Rerun this script as sudo"
+  exit 1
+fi
+
+ln -sf $PWD/superdesk-dev.sh /usr/local/bin/sd
